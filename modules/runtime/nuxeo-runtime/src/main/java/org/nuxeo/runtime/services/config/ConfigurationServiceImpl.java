46abccc94e82b216532e4d85678fa0a08258bf20
Antoine Taillefer <ataillefer@nuxeo.com>
2024-01-16 13:54:35 +0100

    NXP-32229: Format

--- modules/runtime/nuxeo-runtime/src/main/java/org/nuxeo/runtime/services/config/ConfigurationServiceImpl.java
+++ modules/runtime/nuxeo-runtime/src/main/java/org/nuxeo/runtime/services/config/ConfigurationServiceImpl.java
@@ -69,7 +69,8 @@ public class ConfigurationServiceImpl extends DefaultComponent implements Config
     /**
      * XXX remove once we are able to get such a cached map from DefaultComponent.
      * <p>
-     * We'd ideally need a &lt;T extends Descriptor&gt; Map&lt;String, T&gt; getDescriptors(String xp) with cache method.
+     * We'd ideally need a &lt;T extends Descriptor&gt; Map&lt;String, T&gt; getDescriptors(String xp) with cache
+     * method.
      *
      * @since 10.3
      */
