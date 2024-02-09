09627f01ef4d128a5a15b933f28fdf883f25b03e
Nour AL KOTOB <nour.alkotob@hyland.com>
2024-02-09 16:59:46 +0100

    NXP-32221: Log the reason why a document can not be removed

--- modules/core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/AbstractSession.java
+++ modules/core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/AbstractSession.java
@@ -1482,6 +1482,7 @@ public abstract class AbstractSession implements CoreSession, Serializable {
         try {
             checkCanRemoveDocument(doc);
         } catch (NuxeoException e) {
+            log.warn(e::getMessage);
             return false;
         }
         return true;
